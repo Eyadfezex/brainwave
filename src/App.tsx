d864642fde8c0cc2d6cb5159599d873d61155756
Eyad ahmed <Eyad932004@outloo.com>
2024-04-18 08:57:08 +0200

    create Hero section & add to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ButtonGradient } from "./assets/svg/ButtonGradient.jsx";
 import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
 function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
