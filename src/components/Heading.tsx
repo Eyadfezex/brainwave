0ecf7ee21dccf6ffdfd04f37c2fc129dfee11ae8
Eyad ahmed <Eyad932004@outloo.com>
2024-04-25 21:23:11 +0200

    edit Heading component to be more reuseable

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,13 @@
 interface props {
   className?: string;
   title?: string;
+  text?: string;
 }
-const Heading = ({ title, className }: props) => {
+const Heading = ({ title, className, text }: props) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-12`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className=" body-2 mt-4 text-n-4 ">{text}</p>}
     </div>
   );
 };
